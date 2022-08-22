cfdc6bd605d579a5c744443415aa086f2cb3db8b
Jurij Balant <jbalant1@contractor.worldremit.com>
2022-08-22 11:45:51 +0200

    Initial project commit

--- vars/listGithubFiles.groovy
+++ vars/listGithubFiles.groovy
@@ -13,8 +13,9 @@ def call() {
 
         stage('Create') {
             println("Create jobs from DSL scripts ...")
-            folder('folder-a') {
-                description('Folder containing all jobs for folder-a')
+            //folder('folder-a') { description('Folder containing all jobs for folder-a')  }
+            steps{
+                jobDsl scriptText: "folder('New Folder')"
             }
         }
     }
